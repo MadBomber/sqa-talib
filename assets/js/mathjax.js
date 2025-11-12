25370a5cf78f32d768819a4effcb3c285014f305
GitHub Action <action@github.com>
2025-11-12 22:17:47 +0000

    Deployed 108ba83 with MkDocs version: 1.6.1

---
+++ assets/js/mathjax.js
@@ -0,0 +1,18 @@
+/* MathJax Configuration for SQA TAI Documentation */
+
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+
+document$.subscribe(() => {
+  MathJax.typesetPromise()
+})
